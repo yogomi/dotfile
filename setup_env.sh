45cb5d08a7417bb2bf69755d9e0a5961ec5774d7
Makoto Yano <yan133@gmail.com>
2013-06-25 04:49:46 +0900

    Add Neobundle setup script.

--- setup_env.sh
+++ setup_env.sh
@@ -3,7 +3,10 @@
 
 PWD=`pwd`
 BACKUP_DIR=${PWD}/backup
-DOTFILES=".vimrc .zshrc"
+DOTFILES=".vimrc .zshrc .vim"
+
+NEOBUNDLE_GIT_URL="git://github.com/Shougo/neobundle.vim"
+NEOBUNDLE_DIST_DIR="${PWD}/.vim/bundle/neobundle.vim"
 
 echo ${PWD}
 
@@ -11,3 +14,7 @@ for file in ${DOTFILES}
 do
     ln -sf ${PWD}/${file} ${HOME}/${file}
 done
+
+mkdir -p .vim/bundle
+git clone ${NEOBUNDLE_GIT_URL} ${NEOBUNDLE_DIST_DIR}
+
