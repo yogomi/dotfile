71a2003b9765937f7d53855e21886f1bc9e0a74b
矢野亮 <yan@littlesmith.local>
2022-09-08 10:19:32 +0900

    nodeのバージョンを会社で使ってるものを標準にした

--- .zsh/.zrc.node.js.zsh
+++ .zsh/.zrc.node.js.zsh
@@ -4,5 +4,5 @@ else
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-  nvm use --delete-prefix v10.17.0 --silent
+  nvm use --delete-prefix v14.20.0 --silent
 fi
