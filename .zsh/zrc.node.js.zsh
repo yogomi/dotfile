229701d80a21570da57e265b510de43023c180d8
Makoto Yano <yan133@gmail.com>
2023-02-03 15:06:52 +0700

    Nodeのバージョンを18.14.0を標準とした。

--- .zsh/zrc.node.js.zsh
+++ .zsh/zrc.node.js.zsh
@@ -2,5 +2,5 @@ export NVM_DIR="$HOME/.nvm"
 if [ -e "$NVM_DIR/nvm.sh" ]; then
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-  nvm use --delete-prefix v16.17.0 --silent
+  nvm use --delete-prefix v18.14.0 --silent
 fi
